9686e82674ae5455f30f63aba7586050ec09a2f4
Patrick Gross <bears_lair@protonmail.com>
2024-04-17 15:14:33 -0700

    created condition for empty array

--- 2to1String.js
+++ 2to1String.js
@@ -1,11 +1,20 @@
-//1. concat the 2 strings to 1
-//2. convert the string to concatArray
-//3. remove duplicates
+//1. concat the 2 strings to 1 array (concatArray)
+//2. remove duplicates
 //a. declare empty newArray
 //b. push character from old array to empty IF new character not present in newArray
 //sort string
 
-function longest(s1, s2) {}
+function longest(s1, s2) {
+  const concatArray = s1.concat(s2).split("");
+  const newArray = [];
+  for (let i = 0; i < concatArray.length; i++) {
+    if ((newArray = [])) {
+      newArray.push(concatArray[i]);
+    } else {
+    }
+  }
+  console.log(newArray);
+}
 
 console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
 console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
