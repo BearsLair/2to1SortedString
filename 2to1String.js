d8ee3ca6cd0872d4499572bd136e17da30d58e04
Patrick Gross <bears_lair@protonmail.com>
2024-04-17 16:29:06 -0700

    script complete

--- 2to1String.js
+++ 2to1String.js
@@ -6,20 +6,17 @@
 
 function longest(s1, s2) {
   const concatArray = s1.concat(s2).split("");
-  console.log("combined array...", concatArray);
   const newArray = [];
   for (let i = 0; i < concatArray.length; i++) {
     if (newArray.includes(concatArray[i]) === true) {
-      console.log("letter already in array...", concatArray[i]);
       null;
     } else {
       newArray.push(concatArray[i]);
-      console.log("letter pushed to newArray...", concatArray[i]);
     }
   }
-  console.log("no duplicate array...", newArray);
+  return newArray.sort().join("");
 }
 
 console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
-// console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
-// console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
+console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
